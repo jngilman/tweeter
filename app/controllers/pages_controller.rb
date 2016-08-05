c91f96e81569ed3afd39467e6ee2fd1e0ca08e25
James Gilman <jngilman@mac.com>
2016-08-05 11:30:55 -0400

    modified 2 controllers and modified associated views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,39 +3,23 @@ class PagesController < ApplicationController
   end
 
 
-  def new
-    @post = Post.new
-  end
-
   def home
     @posts = Post.all
   end
-
-
+    
   def profile
-    @post = Post.new(post_params)
     if(User.find_by_username(params[:id]))
       @username = params[:id]
     else
       redirect_to root_path, :notice => "User not found!"
     end  
-    @post = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
-    
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+    @newPost = Post.new
   end
 
   def explore
     @posts = Post.all
   end
 
-  def settings
-  end
 
-  def friends
-  end
-
-  
-private
-  def post_params
-    params.require(:post).permit(:user_id, :content)
-  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,15 @@
 class PostsController < ApplicationController
   def new
+  	@post = Post.new
   end
 
   def create
+  	@post = Post.new(post_params)
+  			
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:user_id, :content)
   end
 end
